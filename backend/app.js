ea6c96d9ab6ef58873650cab6eea06d6940df434
LiveMASII <saenzmarcjr@gmail.com>
2024-12-18 16:09:17 -0800

    Get the Current User

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { ValidationError } = require('sequelize');
-
+const { restoreUser } = require('./utils/auth'); 
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -15,62 +15,63 @@ const app = express();
 
 const routes = require('./routes');
 
-
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
 if (!isProduction) {
-    app.use(cors());
-  }
-  
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
-  
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
+  app.use(cors());
+}
 
-  app.use(routes);
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: 'cross-origin',
+  })
+);
 
-  app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.title = "Resource Not Found";
-    err.errors = { message: "The requested resource couldn't be found." };
-    err.status = 404;
-    next(err);
-  });
+app.use(restoreUser);
 
-  app.use((err, _req, _res, next) => {
-    if (err instanceof ValidationError) {
-      let errors = {};
-      for (let error of err.errors) {
-        errors[error.path] = error.message;
-      }
-      err.title = 'Validation error';
-      err.errors = errors;
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && 'Lax',
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = 'Resource Not Found';
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
     }
-    next(err);
-  });
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
 
-  app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-      title: err.title || 'Server Error',
-      message: err.message,
-      errors: err.errors,
-      stack: isProduction ? null : err.stack
-    });
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
   });
+});
 
-  module.exports = app;
+module.exports = app;
